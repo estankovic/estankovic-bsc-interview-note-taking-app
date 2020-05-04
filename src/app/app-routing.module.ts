d61f0f9e4e43c54a8305a72582c9550cc3755b85
Eduard Stankovic <eduard.stankovic@gmail.com>
2020-05-04 18:18:37 +0200

    first screen added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'notes', loadChildren: () => import('./notes/notes.module').then(m => m.NotesModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/notes/notes.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NotesRoutingModule } from './notes-routing.module';
+import { NoteListComponent } from './containers/note-list/note-list.component';
+
+@NgModule({
+  declarations: [NoteListComponent],
+  imports: [CommonModule, NotesRoutingModule]
+})
+export class NotesModule {}
