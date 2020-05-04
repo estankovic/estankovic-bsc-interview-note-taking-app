e92b211d6b3fdd34b434d6d4f7519fec22f9b0f0
Eduard Stankovic <eduard.stankovic@gmail.com>
2020-05-04 18:06:22 +0200

    Added fake backend service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import { Environment } from './environment.interface';
+
+export const environment: Environment = {
+  production: true,
+  backendUrl: 'http://private-anon-c2bbc5f3bc-note10.apiary-mock.com'
 };
